03fbc3e05c1f3ce69bdaf65675314ea28753ac2d
ityuvtrainings <ityuvtrainings@gmail.com>
2026-03-16 14:21:12 +0530

    change about us file

--- src/about.jsx
+++ src/about.jsx
@@ -1,11 +1,12 @@
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import { UserContext } from "./context/userContext"
 
 const About = () => {
+    const [namem,setname]=useState('name')
     const data = useContext(UserContext)
     console.log(data)
     return (<>
-        <h1 style={{backgroundColor:data==='light'?'white':'black',color:data==='light'?'black':'white'}}>Hello {data}</h1>
+        <h1 style={{backgroundColor:data==='light'?'white':'black',color:data==='light'?'black':'white'}}>Hello {data}{namem}</h1>
     </>)
 }
 export default About;
